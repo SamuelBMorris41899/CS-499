ae4a2221da7162204d90d0b0ff96a4e305d2354b
SamuelBMorris41899 <43507180+SamuelBMorris41899@users.noreply.github.com>
2022-01-23 18:44:38 -0600

    2

--- python/tableGenerator.py
+++ python/tableGenerator.py
@@ -3,12 +3,8 @@ from inputProcessor import getAllStatements
 import ttg
 def generateTruthTable(statement):
     v,s = getAllStatements(statement)
-    print(v)
-    for a in s:
-        print(a)
     truthTable = ttg.Truths(list(set(v)),s)
     return (truthTable,truthTable.valuation())
-
 
 # table,blah = generateTruthTable("b or a and ( p and ( a or c ) and ( c or d ) ) or e")
 # table,blah = generateTruthTable("( a and b ) nand c xor ( not ( d or c ) )")
