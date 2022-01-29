55d2b6660581f952947d48cf7f99c74bfe4ca21e
SamuelBMorris41899 <43507180+SamuelBMorris41899@users.noreply.github.com>
2022-01-29 12:07:24 -0600

    All Changes

---
+++ python/recursion.py
@@ -0,0 +1,71 @@
+'''
+11:18 AM - 11:33 AM
+1 hour
+4:05 PM - 4:30 PM
+'''
+
+steps = []
+given = {}
+letter = ""
+
+def getFunctions(statement):
+    if letter not in statement:
+        return []
+    index = statement.find(letter)
+    endIndex = statement.find(")", index)
+    return [statement[index + 1:endIndex + 1]] + getFunctions(statement[endIndex:])
+
+
+def addValueToGiven(n,function,functions):
+    step = ""
+    if n < 0:
+        print("ERROR: bad recursion level")
+        return 0
+
+    newF = function
+    functionWithValue = function
+
+    for f in functions:
+        fValue = eval(f)
+
+        key = letter + "(" + str(fValue) + ")"
+
+        if key not in given.keys():
+            given[key] = addValueToGiven(fValue,function,functions)
+
+        toRepalce = letter + f
+        newF = newF.replace(toRepalce,str(given[key]) )
+        functionWithValue = functionWithValue.replace(toRepalce,key)
+    finalValue = eval(newF)
+    k = letter + "("+str(n)+")"
+    if k not in given.keys():
+        step += "\n\ngiven we know : \n"
+        for key,value in given.items():
+            step += key + " =\t" + str(value) + "\n"
+        step += "and n = {}. the function {} or ({}), we know S({}) = {}".format(n,function,functionWithValue,n,finalValue)
+        given[k] = finalValue
+    steps.append(step)
+    return  finalValue
+
+def evaluateRecursive(function,g,n):
+    global given,steps,letter
+
+    given = g
+    steps = []
+    letter = list(given.keys())[0][0]
+    functions = getFunctions(function)
+    print("final", addValueToGiven(n, function, functions))
+    print(given)
+
+def getSteps(base_function, given_Values, go_to_n):
+    # evaluateRecursive(base_function, given_Values, go_to_n)
+    for pos in range(len(steps)):
+        step = steps[pos]
+        step = "STEP {}\n".format(pos + 1) + step
+        steps[pos] = step
+    return steps
+
+def getValues():
+    l = list(given.values())
+    print(l)
+    return l
--- python/recursionTest.py
+++ python/recursionTest.py
@@ -1,13 +1,39 @@
+import recursion
 class test:
-    given = [
-        ("S1",10)
-    ]
-    function = "S(n-1) + 10"
-    def __init__(self,givens,f):
+    given = {}
+    function = ""
+    def __init__(self,givens,f,n):
         self.given = givens
         self.function = f
+        self.n = n
+    def runTest(self,expected):
+        recursion.evaluateRecursive(self.function, self.given, self.n)
+        if(expected != recursion.getValues()):
+            print ("Failed")
+            return False
+        print("Passed")
+        return True
+
+test_example1 = test(
+    {"S(1)":2},
+    "2 * S(n-1)",
+    5
+)
+test_example1.runTest([2,4,8,16,32])
+
+test_practice2 = test(
+    {"S(1)":1,
+     "S(2)":1},
+    "S(n-1) + S(n-2)",
+    6
+)
+test_practice2.runTest([1,1,2,3,5,8])
+
 
 test1 = test(
-    [("S1",10)],
-    "S(n-1) + 10"
+    {"S(1)":10},
+    "S(n-1) + 10",
+    6
 )
+
+# test1.runTest([10,20,30,40,50,60])
